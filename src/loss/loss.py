b8d83e57d985a3a15a4c88992d3319b087ecfb52
adria <evabot505@gmail.com>
2022-11-15 15:20:55 +0100

    some losses

---
+++ src/loss/loss.py
@@ -0,0 +1,68 @@
+from src.utils.metrics import CosineSimilarityMatrix
+
+import torch
+import torch.nn as nn
+from typing import *
+
+class MatchDistanceLoss(nn.Module):
+    def __init__(self, similarity: Callable = CosineSimilarityMatrix, p_norm: int = 2, margin: float = 0, *args, **kwargs) -> None:
+        super(MatchDistanceLoss).__init__()
+        self.similarity = similarity
+        self.p_norm = p_norm
+        self.margin = margin
+    
+    def forward(self, h, gt):
+        return match_distance_loss(h, gt, self.similarity, self.p_norm, self.margin)
+
+class MatchTopologyLoss(nn.Module):
+    pass
+
+def match_distance_loss(features: torch.tensor, gt_distances: torch.tensor, similarity: Callable = CosineSimilarityMatrix, p_norm: int = 2, margin: float = 0) -> torch.tensor:
+    '''
+
+    features: Torch.tensor: (bs, feature_size)
+    gt_distances Torch.tensor: (bs, bs)
+
+    '''
+    if isinstance(similarity, CosineSimilarityMatrix):
+        h_distances = 1 - similarity(features, features) # Shape: (Bs x Bs)
+    else: raise NotImplementedError
+
+    mask_diagonal = ~ torch.eye(h_distances.shape[0]).bool()
+    h_distances_eyed = h_distances[mask_diagonal].view(-1) # Shape: (Bs-1*Bs-1)?
+    # We remove the diagonal
+
+    gt = gt_distances[mask_diagonal].view(-1)
+
+    # Compute distance and norm
+    sqrd = (gt - h_distances_eyed)**p_norm
+
+    loss = sqrd.sum(-1) ** (1/p_norm) + margin # Limitation of this, you are immitating distances, not topology.
+
+    return loss
+
+def minimum_transform_loss(features: torch.tensor, gt_distances: torch.tensor, similarity: Callable = CosineSimilarityMatrix, eye: bool = False) -> torch.tensor:
+    '''
+
+    features: Torch.tensor: (bs, feature_size)
+    gt_distances Torch.tensor: (bs, bs)
+
+    '''
+    if isinstance(similarity, CosineSimilarityMatrix):
+        h_distances = 1 - similarity(features, features) # Shape: (Bs x Bs)
+    else: raise NotImplementedError
+
+    if eye:
+        mask_diagonal = ~ torch.eye(h_distances.shape[0]).bool()
+        h_distances_eyed = h_distances[mask_diagonal].view(h_distances.shape[0], h_distances.shape[0] - 1) # Shape: (Bs x Bs-1)
+        gt_distances_eyed = gt_distances[mask_diagonal].view(h_distances.shape[0], h_distances.shape[0] - 1) # Shape: (Bs x Bs-1)
+    else:
+        # I'm not sure about the consequences of this
+        h_distances_eyed = h_distances
+        gt_distances_eyed = gt_distances
+
+
+    # Calculate transformation matrix
+    T = torch.matmul(torch.linalg.pinv(h_distances_eyed), gt_distances_eyed)
+    # Ara el loss hauria d'expresar com de "dura" es la matriu de transformació, si és tot 1s genial, si no, problema
+    raise NotImplementedError
---
+++ src/utils/metrics.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+import torch
+from torch import Tensor
+from typing import *
+
+class CosineSimilarityMatrix(nn.Module):
+    name = 'cosine_matrix'
+    def __init__(self, dim: int = 1, eps: float = 1e-8) -> None:
+        super(CosineSimilarityMatrix, self).__init__()
+        self.dim = dim
+        self.eps = eps
+
+    def forward(self, x1: Tensor, x2: Tensor) -> Tensor:
+        return cosine_similarity_matrix(x1, x2, self.dim, self.eps)
+
+def cosine_similarity_matrix(x1: Tensor, x2: Tensor, dim: int = 1, eps: float = 1e-8) -> Tensor:
+    '''
+    When using cosine similarity the constant value must be positive
+    '''
+    #Cosine sim:
+    xn1, xn2 = torch.norm(x1, dim=dim), torch.norm(x2, dim=dim)
+    x1 = x1 / torch.clamp(xn1, min=eps).unsqueeze(dim)
+    x2 = x2 / torch.clamp(xn2, min=eps).unsqueeze(dim)
+    x1, x2 = x1.unsqueeze(0), x2.unsqueeze(1)
+
+    sim = torch.tensordot(x1, x2, dims=([2], [2])).squeeze()
+
+    sim = (sim + 1)/2 #range: [-1, 1] -> [0, 2] -> [0, 1]
+
+    return sim
