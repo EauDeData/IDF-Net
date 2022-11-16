86f654819f509bdfe29776fe4ec9d9390a414428
adria <evabot505@gmail.com>
2022-11-16 12:33:41 +0100

    loss and dataset ignore

--- src/loss/loss.py
+++ src/loss/loss.py
@@ -4,20 +4,34 @@ import torch
 import torch.nn as nn
 from typing import *
 
-class MatchDistanceLoss(nn.Module):
-    def __init__(self, similarity: Callable = CosineSimilarityMatrix, p_norm: int = 2, margin: float = 0, *args, **kwargs) -> None:
-        super(MatchDistanceLoss).__init__()
+class CustomLoss:
+    def __init__(self) -> None:
+        pass
+    def __call__(self, h, gt): return self.forward(h, gt)
+
+class NormLoss(CustomLoss):
+    def __init__(self, similarity: Callable = CosineSimilarityMatrix(), p_norm: int = 2, margin: float = 0, procrustes = True, *args, **kwargs) -> None:
         self.similarity = similarity
         self.p_norm = p_norm
         self.margin = margin
+        self.orthogonality_solver = procrustes
     
     def forward(self, h, gt):
-        return match_distance_loss(h, gt, self.similarity, self.p_norm, self.margin)
+        return norm_loss(h, gt, self.similarity, self.p_norm, self.margin, self.orthogonality_solver)
+
+class PearsonLoss(CustomLoss):
+    def __init__(self,  similarity: Callable = CosineSimilarityMatrix(), p_norm: int = 2) -> None:
+        self.similarity = similarity
+        self.p_norm = p_norm
+    
+    def forward(self, h, gt):
+        return pearson_correlation_loss(h, gt_distances=gt, similarity=self.similarity, p_norm=self.p_norm)
+
 
 class MatchTopologyLoss(nn.Module):
     pass
 
-def match_distance_loss(features: torch.tensor, gt_distances: torch.tensor, similarity: Callable = CosineSimilarityMatrix, p_norm: int = 2, margin: float = 0) -> torch.tensor:
+def norm_loss(features: torch.tensor, gt_distances: torch.tensor, similarity: Callable = CosineSimilarityMatrix(), p_norm: int = 2, margin: float = 0, orth = True) -> torch.tensor:
     '''
     This loss proposes an ismoetry between spaces. We want to measure isomorfism and minimize it.
     Distances1 = Distances2
@@ -25,23 +39,31 @@ def match_distance_loss(features: torch.tensor, gt_distances: torch.tensor, simi
         gt_distances Torch.tensor: (bs, bs)
 
     '''
-    if isinstance(similarity, CosineSimilarityMatrix):
+    if similarity.name == 'cosine_matrix':
         h_distances = 1 - similarity(features, features) # Shape: (Bs x Bs)
     else: raise NotImplementedError
 
     mask_diagonal = ~ torch.eye(h_distances.shape[0]).bool()
-    h_distances_eyed = h_distances[mask_diagonal].view(-1) # Shape: (Bs-1*Bs-1)?
+    h_distances_eyed = h_distances[mask_diagonal].view(gt_distances.shape[0], gt_distances.shape[0]-1) # Shape: (Bs-1*Bs-1)?
     # We remove the diagonal
 
     gt = gt_distances[mask_diagonal].view(-1)
 
     # Compute distance and norm
-    sqrd = (gt - h_distances_eyed)**p_norm
-
-    loss = sqrd.sum(-1) ** (1/p_norm) + margin # Limitation of this, you are immitating distances, not topology.
-
+    if orth:
+        X = torch.matmul(h_distances_eyed, h_distances_eyed.T)
+        V, _, U = torch.linalg.svd(X) # Why X? How do we deal with Non-Square matrices?
+        W = torch.matmul(U, V.T)
+        sqrd = (torch.matmul(W, h_distances_eyed).view(-1) - gt)**p_norm
+    else: sqrd = (h_distances_eyed - gt)**p_norm
+    loss = torch.sum(sqrd) ** (1/p_norm) + margin # Limitation of this, you are immitating distances, not topology.
     return loss
 
+def pearson_correlation_loss(features: torch.tensor, gt_distances: torch.tensor, similarity: Callable = CosineSimilarityMatrix(), p_norm: int = 2):
+
+    # From: https://arxiv.org/pdf/2210.05098.pdf
+    raise NotImplementedError
+    
 def minimum_transform_loss(features: torch.tensor, gt: torch.tensor, similarity: Callable = CosineSimilarityMatrix, eye: bool = False) -> torch.tensor:
     '''
     This loss proposes an homeomorphism between spaces.
--- unitary_tests.py
+++ unitary_tests.py
@@ -1,7 +1,12 @@
+import torch
+import nltk 
+
 from src.text.preprocess import StringCleanAndTrim, StringCleaner
 from src.utils.errors import *
 from src.dataloaders.dataloaders import DummyDataset
 from src.text.map_text import LSALoader, TF_IDFLoader
+from src.loss.loss import NormLoss, PearsonLoss
+nltk.download('stopwords')
 
 if __name__ == '__main__': 
     
@@ -11,7 +16,7 @@ if __name__ == '__main__':
         print(returned)
         if not isinstance(returned, list): raise WrongTypeReturnedeError
     except Exception as e:
-        print(f"Preprocess test not passed, reason: {e}") 
+        print(f"0 - Preprocess test not passed, reason: {e}") 
 
     try:
         dataset = DummyDataset()
@@ -20,7 +25,7 @@ if __name__ == '__main__':
         loader.fit()
         print(loader[0])
     except Exception as e:
-        print(f"Preprocess test not passed, reason: {e}")
+        print(f"1 - Preprocess test not passed, reason: {e}")
     try:
         dataset = DummyDataset()
         cleaner = StringCleanAndTrim()
@@ -28,7 +33,18 @@ if __name__ == '__main__':
         loader.fit()
         print(loader[0])
     except Exception as e:
-        print(f"Preprocess test not passed, reason: {e}")
+        print(f"2 - Preprocess test not passed, reason: {e}")
+
+    try:
+        loss2 = NormLoss()
+
+        h = torch.rand((5, 10), requires_grad = True)
+        gt = torch.ones((5, 5))
+
+        print(loss2(h, gt))
+    
+    except Exception as e:
+        print(f"3 - Loss test not passed, reason: {e}")
     
 
     
