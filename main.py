5338b8dfa304189907bf5ff23386ae51f04894f7
EauDeData <amolina@cvc.uab.cat>
2022-12-19 19:50:50 +0100

    glove loader

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ dataset = AbstractsDataset('/home/adri/Downloads/archive/arxiv_data.csv', './dat
 ### On which we clean the text and load the tokenizer ###
 print("Tokenizing text!")
 cleaner = StringCleanAndTrim()
-loader = LSALoaderGLOVE(dataset, StringCleaner())
+loader = LSALoader(dataset, StringCleaner())
 loader.fit()
 
 ### Now we setup the tokenizer on the dataset ###
