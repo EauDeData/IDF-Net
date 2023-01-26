4548c26920959f02f7a2dd988a2fb8555cad692a
adria <evabot505@gmail.com>
2023-01-26 13:22:20 +0100

    uwuuu

--- src/models/models.py
+++ src/models/models.py
@@ -2,6 +2,7 @@ import torch
 from vit_pytorch import ViT
 
 
+
 class VisualTransformer(torch.nn.Module):
     def __init__(self, image_size, patch_size = 32, embedding_size = 128, depth = 1, heads = 1, dropout = 0.1) -> None:
         super(VisualTransformer, self).__init__()
@@ -21,6 +22,53 @@ class VisualTransformer(torch.nn.Module):
     
     def forward(self, batch):
         return self.extractor(batch)
+
+
+class SelfAttention(torch.nn.Module):
+    """ Self attention Layer"""
+    # Source: https://discuss.pytorch.org/t/attention-in-image-classification/80147/3
+    def __init__(self,in_dim, activation):
+        super(SelfAttention,self).__init__()
+        self.chanel_in = in_dim
+        self.activation = activation
+        
+        self.query_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1)
+        self.key_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1)
+        self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax  = nn.Softmax(dim=-1) 
+
+    def forward(self,x):
+        """
+            inputs :
+                x : input feature maps( B X C X W X H)
+            returns :
+                out : self attention value + input feature 
+                attention: B X N X N (N is Width*Height)
+        """
+        m_batchsize,C,width ,height = x.size()
+        proj_query  = self.query_conv(x).view(m_batchsize,-1,width*height).permute(0,2,1) # B X CX(N)
+        proj_key =  self.key_conv(x).view(m_batchsize,-1,width*height) # B X C x (*W*H)
+        energy =  torch.bmm(proj_query,proj_key) # transpose check
+        attention = self.softmax(energy) # BX (N) X (N) 
+        proj_value = self.value_conv(x).view(m_batchsize,-1,width*height) # B X C X N
+
+        out = torch.bmm(proj_value,attention.permute(0,2,1) )
+        out = out.view(m_batchsize,C,width,height)
+        
+        out = self.gamma*out + x
+        return out,attention
+
+class VisualConvAttention(torch.nn.Module):
+    def __init__(self,  embedding_size = 128): # TODO: Relevant hyperparameters
+
+        self.extractor = torch.nn.Sequential(*list(resnet50.children())[:-2])
+        self.attention = SelfAttention()
+
+    def forward(self, x):
+        pass
+
 
 class Resnet50(torch.nn.Module):
     def __init__(self, embedding_size = 128):
