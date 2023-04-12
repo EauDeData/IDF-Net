36da3ea868825b0477209cd182b1644169caf65f
eaudedata <evabot505@gmail.com>
2023-04-12 11:54:25 +0200

    linux modules instalation

---
+++ lniuxmodules.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get install texlive-latex-base -y
+sudo apt-get install texlive-fonts-recommended -y
+sudo apt-get install texlive-fonts-extra -y
+sudo apt-get install texlive-latex-extra -y
+sudo apt-get install texlive-extra-utils  -y
+ sudo apt-get install -y pnmtopng 
