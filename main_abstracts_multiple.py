4e80bd5f6c99b0d073e2ba57f19fed81a134dcc1
amolina <evabot505@gmail.com>
2023-05-23 15:05:10 +0200

    i commit bc i fear losing useless progress

--- main_abstracts_multiple.py
+++ main_abstracts_multiple.py
@@ -11,7 +11,8 @@ from src.text.map_text import LSALoader, TF_IDFLoader, LDALoader, CLIPLoader, Be
 from src.loss.loss import PairwisePotential, NNCLR, SpearmanRankLoss, MSERankLoss
 from src.models.models import VisualTransformer, Resnet50, Resnet, ResNetWithEmbedder, AbstractsTopicSpotter
 from src.dataloaders.dataloaders import AbstractsDataset, AbstractsAttn
-from src.tasks.tasks import Train, Test, TestMiniClip, TrainMiniClip
+from src.tasks.tasks import Train, Test
+from src.tasks.train_doc import TrainDocAbstracts
 from src.tasks.evaluation import MAPEvaluation
 from src.dataloaders.annoyify import Annoyifier
 nltk.download('stopwords')
@@ -20,9 +21,9 @@ torch.manual_seed(42)
 # TODO: Use a config file
 # Some constants
 IMSIZE = 224
-DEVICE = 'cuda' # TODO: Implement cuda execution
+DEVICE = 'cuda:1' # TODO: Implement cuda execution
 BSIZE = 64
-DEVICE_BERT = 'cuda:5'
+DEVICE_BERT = 'cuda:1'
 bert = BertTextEncoder().to(DEVICE_BERT)
 
 dataset = AbstractsAttn('train_set.csv', 'dataset/arxiv_images_train/', bert = bert)
@@ -51,9 +52,9 @@ model = AbstractsTopicSpotter(model_visual, emb_size = 224, out_size=128,)
 optim = torch.optim.Adam(model.parameters(), lr = 5e-4)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optim, 'min')
 
-test_task = Test(dataset_test, model, loss_function, loader, None, optim, loader, scheduler = scheduler, device = DEVICE, bsize = BSIZE)
-train_task = Train(dataset, model, loss_function, loader, None, optim, test_task,loader, device= DEVICE, bsize = BSIZE)
+task = TrainDocAbstracts(dataset, dataset_test, model, None, loss_function, None, None, optim, None, bsize=5, device='cuda', workers=16)
 
-train_task.run(epoches = 120)
+
+task.train(epoches = 120)
 
 
