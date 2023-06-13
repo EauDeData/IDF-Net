365e9f057e66442203fe5f103e1f2b9198217484
amolina <evabot505@gmail.com>
2023-06-13 16:28:14 +0200

    attentions added

--- main_abstracts_multiple.py
+++ main_abstracts_multiple.py
@@ -66,7 +66,7 @@ dataset_test.tokenizer = loader
 OUT_SIZE = 128
 loss_function = KullbackDivergenceWrapper()
 model_visual = Resnet50(OUT_SIZE, norm = 2) # VisualTransformer(IMSIZE)
-model = AbstractsTopicSpotter(model_visual, emb_size = OUT_SIZE, out_size=512, bert_size=224) # For now we condition with the idf itself
+model = AbstractsTopicSpotter(model_visual, emb_size = OUT_SIZE, out_size=224, bert_size=224) # For now we condition with the idf itself
 
 ### Optimizer ###
 optim = torch.optim.Adam(model.parameters(), lr = 5e-5)
